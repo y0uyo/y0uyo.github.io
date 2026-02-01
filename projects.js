59d93ca6cce99f8b026157acaf7db87ffcb8f5b4
y0uyo <140636399+y0uyo@users.noreply.github.com>
2026-02-01 01:48:04 +0100

    update

---
+++ projects.js
@@ -0,0 +1,245 @@
+    const projects = [
+      {
+        title: "YOU Run",
+        year: "2019",
+        description: "An endless runner game. Started development at age 14 and released it on the Google Play store in 2020 when I was 15, still working on it sometimes when I feel like it. I still think it has potential.",
+        banner: "./Media/yr/banner4.png",
+        links: [
+          { label: "App Store", url: "https://apps.apple.com/ch/app/you-run-404/id6745303381?l=en-GB" },
+          { label: "Google Play Store v1", url: "https://play.google.com/store/apps/details?id=com.YoussefIbrahim.DashRush" },
+          { label: "Google Play Store v2", url: "https://play.google.com/store/apps/details?id=com.yi.YOURun404" },
+          { label: "Amazon Store", url: "https://www.amazon.de/YOUssef-YOU-Run/dp/B0BWCG4Z11" },
+          { label: "itch.io", url: "https://yyyi.itch.io/yourun404" },
+          { label: "YouTube", url: "https://www.youtube.com/@YOURun404" },
+          { label: "TikTok", url: "https://www.tiktok.com/@yourun404" },
+          { label: "FaceBook", url: "https://www.facebook.com/Y0URun" }
+
+        ],
+        media: [
+          "https://youtu.be/L-iRTHisp10",
+          "https://youtu.be/ndYvDv4_0oM",
+          "https://youtu.be/NIuoOfR0gv4",
+          "https://www.youtube.com/watch?v=1_9u4jxZ6CE",
+          "https://www.youtube.com/watch?v=tJzPpncPvxQ&t",
+          "https://raw.githubusercontent.com/y0uyo/YOURun/main/Media/YOU Run Running Game.jpg",
+          "https://raw.githubusercontent.com/y0uyo/YOURun/main/Media/old_YOURun_Icon_512px.png",
+          "https://raw.githubusercontent.com/y0uyo/YOURun/main/Media/banner1.webp",
+          "https://raw.githubusercontent.com/y0uyo/YOURun/main/Media/512x512.png"
+        ]
+      },
+
+      {
+        title: "The Snaliens",
+        year: "2022",
+        description: "A collection of 10'000 snaliens. An NFT Collection on the Ethereum blockchain.",
+        banner: "./Media/ts/banner3.png",
+        links: [
+          { label: "View Gallery", url: "snaliens-gallery.html"},
+          { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+          { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+          { label: "X.com", url: "https://x.com/TheSnaliens"},
+          { label: "Website (archive)", url: ""},
+          { label: "definition of a snalien (article)", url: "https://paragraph.com/@the-snaliens/definition-of-a-snalien"},
+        ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "ardge wear",
+        year: "2022",
+        description: "started a streetwear brand with my friend fahham. created a few samples, did photoshoots, started marketing, but never dropped. too much planning and no execution.",
+        banner: "./Media/aw/trucker_carrousel.gif",
+        links: [
+          { label: "Instagram", url: "https://www.instagram.com/ardgewear/"},
+          { label: "TikTok", url: "https://www.tiktok.com/@ardgewear"},
+          { label: "Pinterest", url: "https://pinterest.com/ardgewear/"},
+        ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "idolica",
+        year: "aug 2022 - july 2023",
+        description: "An E-commerce Store selling self cleaning hairbrushes",
+        banner: "./Media/others/idolica.png",
+       // links: [
+       //   { label: "View Gallery", url: "snaliens-gallery.html"},
+       //   { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+       //   { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+       //   { label: "X.com", url: "https://x.com/TheSnaliens"},
+       //   { label: "Website (archive)", url: ""}
+       // ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "LoopyBrush",
+        year: "jan 2024 - feb 2025",
+        description: "An E-commerce Store selling self cleaning hairbrushes.",
+        banner: "./Media/others/loopybrush.png",
+      //  links: [
+      //    { label: "View Gallery", url: "snaliens-gallery.html"},
+      //    { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+      //    { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+      //    { label: "X.com", url: "https://x.com/TheSnaliens"},
+      //    { label: "Website (archive)", url: ""}
+      //  ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "DopamineBay",
+        year: "2024",
+        description: "An E-commerce Store case study for a final school project. No revenue generated due to tight deadlines, there was no time for testing. Total loss about -150 USD. Grade 6/6. The last ecom business, where I didn't have quality control over my products. Decided afterwards to pursue ventures, where I can stand behind what I sell or create and have total control over the quality, despite less rapid success and profit.",
+        banner: "./Media/others/dopaminebay.png",
+    //    links: [
+    //      { label: "View Gallery", url: "snaliens-gallery.html"},
+    //      { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+    //      { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+    //      { label: "X.com", url: "https://x.com/TheSnaliens"},
+    //      { label: "Website (archive)", url: ""}
+    //    ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "GRAPHIC DESIGN",
+        year: "all my life",
+        description: "started creating designs, 3d characters and logos for gamers in photoshop and cinema4d (2015). Then started selling logos on fiverr (2018). Later designed various flyers, other print media and websites for Restaurants.",
+        banner: "./Media/others/graphicdesign.png",
+        //links: [
+        //  { label: "View Gallery", url: "snaliens-gallery.html"},
+        //],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+      {
+        title: "BALANCE ROBOT",
+        year: "2021",
+        description: "Self Balancing Robot created for a school project.",
+        banner: "./Media/others/balance.png",
+        links: [
+          { label: "video", url: "https://youtu.be/jHVGEODFmCk"},
+        ],
+        media: [
+          "https://youtu.be/jHVGEODFmCk"
+        ]
+      },
+
+      {
+        title: "ART",
+        year: "all my life",
+        description: "Drawings, Scupltures",
+        banner: "./Media/art/moving_head.gif",
+        // links: [
+        //   { label: "View Gallery", url: "snaliens-gallery.html"},
+        //   { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+        //   { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+        //   { label: "X.com", url: "https://x.com/TheSnaliens"},
+        //   { label: "Website (archive)", url: ""}
+        // ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+      {
+        title: "9-5 PROJECTS",
+        year: "2021 - 2026",
+        description: "Projects I did during my apprentice ship at work or in school.",
+        banner: "./Media/9-5/9-5.png",
+        // links: [
+        //   { label: "View Gallery", url: "snaliens-gallery.html"},
+        //   { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+        //   { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+        //   { label: "X.com", url: "https://x.com/TheSnaliens"},
+        //   { label: "Website (archive)", url: ""}
+        // ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+      {
+        title: "RANDOM",
+        year: "2018 - 2020",
+        description: "Experimentations I created as a child",
+        banner: "./Media/others/random.png",
+        // links: [
+        //   { label: "View Gallery", url: "snaliens-gallery.html"},
+        //   { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+        //   { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+        //   { label: "X.com", url: "https://x.com/TheSnaliens"},
+        //   { label: "Website (archive)", url: ""}
+        // ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+      {
+        title: "CAR REPAIR",
+        year: "ongoing",
+        description: "fixing my car",
+        banner: "./Media/others/car.gif",
+        // links: [
+        //   { label: "View Gallery", url: "snaliens-gallery.html"},
+        //   { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+        //   { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+        //   { label: "X.com", url: "https://x.com/TheSnaliens"},
+        //   { label: "Website (archive)", url: ""}
+        // ],
+        media: [
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150",
+          "https://via.placeholder.com/150"
+        ]
+      },
+
+  //          {
+  //      title: "YOU RUN INSTA FILTER",
+  //      year: "2021",
+  //      description: "Instagram Face Filter with YOU Run character",
+  //      banner: "./Media/",
+  //      links: [
+  //        { label: "View Gallery", url: "snaliens-gallery.html"},
+  //        { label: "OpenSea", url: "https://opensea.io/collection/snaliens"},
+  //        { label: "Etherscan", url: "https://etherscan.io/address/0xdd572cfae99a134096333d1eed3e614fe88c2e9e"},
+  //        { label: "X.com", url: "https://x.com/TheSnaliens"},
+  //        { label: "Website (archive)", url: ""}
+  //      ],
+  //      media: [
+  //        "https://via.placeholder.com/150",
+  //        "https://via.placeholder.com/150",
+  //        "https://via.placeholder.com/150"
+  //      ]
+  //    }
+
+    ];
